1c83d6a940d491d54397a5cfc28e0145a032ecec
lee0su <leeys4903@naver.com>
2025-01-09 19:22:02 +0900

    Initial Commit

---
+++ Poker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Poker
+//
+//  Created by 이영수 on 1/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Poker/PokerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokerApp.swift
+//  Poker
+//
+//  Created by 이영수 on 1/9/25.
+//
+
+import SwiftUI
+
+@main
+struct PokerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
